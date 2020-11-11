c560886c2b9856c91932fc8595bea6f5fd19306c
johanickl <49444603+johanickl@users.noreply.github.com>
2020-11-11 09:40:42 +0100

    convergence images
    several images of convergence and self convergence for the given example functions

---
+++ python/finite_differences/derivatives_for_example_functions_plot.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import finite_differences_o2 as dvo2
+import finite_differences_o4 as dvo4
+import example_functions as ex
+
+#####################################################
+#list of functions
+func = ex.func
+#list of 1st analyitcal function derivatives
+func_1st_deriv = ex.func_1st_deriv
+#list of 2nd analyitcal function derivatives
+func_2nd_deriv =  ex.func_2nd_deriv
+#list of functions tex strings
+tex_f = ex.tex_f
+############ set grid ###############################
+n=20       #grid points
+h=1/n       #grid spacing
+x = np.linspace(0,1,n)
+
+#function selection, input options = 0, 1, 2, 3, 4, 5(for g_a)
+ex.a =1
+fs = 5
+############ calculating ##########################
+y = func[fs](x)
+y_prime = func_1st_deriv[fs](x)
+y_prime_prime = func_2nd_deriv[fs](x)
+
+dydx = dvo2.derivative_1_c(func[fs],x,h=h)    #calculate 1st derivative
+dy2dx2 = dvo2.derivative_2_c(func[fs],x,h=h)    #calculate 2nd derivative
+print(dydx)
+Delta_1 = y_prime - dydx              #analytical-difference 1st order
+Delta_2 = y_prime_prime - dy2dx2        #analytical-difference 2nd order
+
+################ plots ###################
+#plt.rc('text', usetex=True)
+
+fig, (ax1, ax2) = plt.subplots(2,figsize=(10, 9))
+
+ax1.set_ylabel('')
+#ax1.set_xlabel('x',loc='center')
+ax1.plot(x,y,label='$f_%d(x)$' %(fs+1))
+ax1.plot(x,y_prime,linestyle='dashed',
+    label="Analytical 1st Derivative $f_%d^{(1)}(x)$" %(fs+1))
+ax1.plot(x,dydx,marker = 'o',linestyle='dotted',
+    label="Central 1st Difference $D^1_h f_%d(x)$" %(fs+1))
+ax1.plot(x,y_prime_prime, linestyle='dashed',
+    label="Analytical 2nd Derivative $f_%d^{(2)}(x)$" %(fs+1))
+ax1.plot(x,dy2dx2,marker = 'o',linestyle='dotted',
+    label="Central 2nd Difference $D^2_h f_%d(x)$" %(fs+1))
+ax1.set_title('function and derivatives for ' '%s'  %(tex_f[fs]))
+ax1.legend( title='grid size n=%d' % (n) )
+ax1.grid(color = 'gainsboro')
+
+ax2.set_ylabel('analytical - finite difference stencil')
+ax2.set_xlabel('x',loc='center')
+ax2.plot(x,Delta_1,marker = 'o', color = 'cyan', label=' 1st order')
+ax2.plot(x,Delta_2, marker = 'o', color= 'hotpink', label=' 2nd order')
+ax2.set_title('derivative difference')
+ax2.legend()
+ax2.grid(color = 'gainsboro')
+
+plt.show()
--- python/finite_differences/finite_differences_o2.py
+++ python/finite_differences/finite_differences_o2.py
@@ -50,7 +50,7 @@ def derivative_2_c(f,a,h=0.05):
 
     return (f(a + h) - 2*f(a) + f(a - h))/(h**2)
 ################### first derivative ##############
-def derivative_1(f,a,method='central',h=0.05):
+def derivative_1_f(f,a,h=0.05):
     '''Compute the difference formula for f'(a) with step size h.
 
     Parameters
@@ -72,17 +72,9 @@ def derivative_1(f,a,method='central',h=0.05):
             forward: f(a+h) - f(a))/h
             backward: f(a) - f(a-h))/h
     '''
-    if method == 'central':
-        return (f(a + h) - f(a - h))/(2*h)
-    elif method == 'forward':
-        return (f(a + h) - f(a))/h
-    elif method == 'backward':
-        return (f(a) - f(a - h))/h
-    else:
-        raise ValueError("Method must be 'central', 'forward' or 'backward'.")
-
+    return (f(a + h) - f(a))/h
 #################### second derivative ###############
-def derivative_2(f,a,method='central',h=0.05):
+def derivative_2_f(f,a,method='central',h=0.05):
     '''Compute the difference formula for f''(a) with step size h.
 
     Parameters
@@ -104,11 +96,4 @@ def derivative_2(f,a,method='central',h=0.05):
             forward: (f(a+2h) - 2f(a+h) + f(a)) / h**2
             backward: (f(a) - 2f(a-h) + f(a-2h)) / h**2
     '''
-    if method == 'central':
-        return (f(a + h) - 2*f(a) + f(a - h))/(h**2)
-    elif method == 'forward':
-        return (f(a + 2*h) - 2*f(a + h) + f(a))/(h**2)
-    elif method == 'backward':
-        return (f(a) - 2*f(a - h) + f(a - 2*h))/(h**2)
-    else:
-        raise ValueError("Method must be 'central', 'forward' or 'backward'.")
+    return (f(a + 2*h) - 2*f(a + h) + f(a))/(h**2)
