c97b0687628db181c6643833eb196ba7724ebdf3
anneaux <a.weber@uni-jena.de>
2020-12-06 01:41:40 +0100

    Implement periodic BC better

--- python/solver.py
+++ python/solver.py
@@ -1,6 +1,4 @@
 import numpy as np
-# from rungekutta.rk4 import rk4
-# from finite_differences.finite_differences_o2 import *
 from finite_differences.example_functions import *
 import matplotlib.pyplot as plt
 
@@ -30,13 +28,16 @@ def wave_evolution1D(phi0,Pi0,timevalues,xvalues):
   Nx = len(xvalues)
   phi = np.zeros([Nt,Nx+2])
   Pi = np.zeros([Nt,Nx+2])
-
+  ### first time step
   phi[0,1:Nx+1] = phi0
-  ### this just copies the ast values to the ghost points
+  ### fill ghost points with values at boundary
   phi[0,0] = phi0[0]
   phi[0,Nx+1] = phi0[Nx-1]
 
-  Pi[0,:] = Pi0
+  Pi[0,1:Nx+1] = Pi0
+  ### fill ghost points with values at boundary
+  Pi[0,0] = Pi0[0]
+  Pi[0,Nx+1] = Pi0[Nx-1]
 
   def time_diff(phi, Pi, t): # where u = [phi, Pi]
     dphidt = Pi # because d/dt phi = Pi
@@ -44,32 +45,45 @@ def wave_evolution1D(phi0,Pi0,timevalues,xvalues):
     d2phidx2= np.zeros(Nx+2)
     for ix in range(1,Nx+1): # computing only inner points
       d2phidx2[ix] = 1/deltax**2 * (phi[ix+1] - 2*phi[ix] + phi[ix-1])
-    d2phidx2[0] = d2phidx2[1]
-    d2phidx2[-1] = d2phidx2[-2]
+    d2phidx2[-1] = d2phidx2[1]
+    d2phidx2[0] = d2phidx2[-2]
     dPidt = c**2 * d2phidx2
     return dphidt, dPidt
 
   t = 1
   # time iteration (RK4 method)
   for i in range(0,Nt-1):
-    k1_phi,k1_Pi  = time_diff(phi[i,:], Pi[i], t)
+    # right boundary
+    phi[i, -1] = phi[i,1]
+    Pi[i, -1] = Pi[i,1]
+    # left boundary
+    phi[i, 0] = phi[i,-2]
+    Pi[i, 0] = Pi[i,-2]
+
+    k1_phi, k1_Pi  = time_diff(phi[i,:], Pi[i], t)
     k2_phi, k2_Pi = time_diff(phi[i,:] + 0.5*deltat*k1_phi,Pi[i,:] + 0.5*deltat*k1_Pi,t + 0.5*deltat)
-    k3_phi,k3_Pi = time_diff(phi[i,:] + 0.5*deltat*k2_phi,Pi[i,:] + 0.5*deltat*k2_Pi ,t + 0.5*deltat)
-    k4_phi,k4_Pi = time_diff(phi[i,:] + deltat*k3_phi,Pi[i,:] + deltat*k3_Pi ,t + deltat)
+    k3_phi, k3_Pi = time_diff(phi[i,:] + 0.5*deltat*k2_phi,Pi[i,:] + 0.5*deltat*k2_Pi ,t + 0.5*deltat)
+    k4_phi, k4_Pi = time_diff(phi[i,:] + deltat*k3_phi,Pi[i,:] + deltat*k3_Pi ,t + deltat)
 
     phi[i+1,:] = phi[i,:] + deltat*(1/6*k1_phi + 1/3*k2_phi +1/3*k3_phi + 1/6*k4_phi)
     Pi[i+1,:] = Pi[i,:] + deltat*(1/6*k1_Pi + 1/3*k2_Pi +1/3*k3_Pi + 1/6*k4_Pi)
-  return phi[:,1:Nx+1], Pi[:,1:Nx+1]
+
+  return phi[:,1:Nx+1], Pi[:,1:Nx+1] # return only inner points
 
 # choose f_4, f_5 or g_a here (for the latter specify a = ...)
 # phi0 = g_a(xvalues,0.025)
 def gaussian(x,sigma,mu):
   return 1/np.sqrt(2*np.pi*sigma**2) * np.exp(- (x-mu)**2/np.sqrt(2*sigma**2))
+def gaussian_drv(x,sigma,mu):
+  return  -(x-mu)/(sigma**2 * np.sqrt(np.pi)) * np.exp(-(x-mu)**2/np.sqrt(2 * sigma**2))
+
+# phi0 = g_a(xvalues,20)#
+# Pi0 = 3*np.zeros(len(phi0))#- g_a_prime(xvalues,20)
+
+### gaussian wave packet
 phi0 = gaussian(xvalues,0.005,0.5)
-
-Pi0 = 0
+Pi0 = -c * gaussian_drv(xvalues,0.005,0.5)
 phi, Pi = wave_evolution1D(phi0,Pi0,timevalues,xvalues)
-
 ############################################
 
 ### Plotting the solutions in a position-time diagram ##############
@@ -119,10 +133,10 @@ ax3.set_ylim(np.amin(phi[:,:]),np.amax(phi[:,:]))
 timelabel = ax3.text(0.02, 0.95, '', transform=ax3.transAxes)
 ani = matplotlib.animation.FuncAnimation(fig3, animate, frames=Nt, blit = True) #init_func=init_animation,
 
-### write as gif
-ani.save('WE-animation.gif', writer='imagemagick', fps=15)
+### write as gif (gif stockt manchmal ein bisschen, geht außerdem sehr langsam zu speichern)
+# ani.save('WE-animation.gif', writer='imagemagick', fps=15)
 
 ### write as mp4
-# Writer = matplotlib.animation.writers['ffmpeg'] # Set up formatting for the movie files
-# mywriter = Writer(fps=15, metadata=dict(artist='AW'), bitrate=1800)
-# ani.save('WE-animation.mp4', writer=mywriter)
+Writer = matplotlib.animation.writers['ffmpeg'] # Set up formatting for the movie files
+mywriter = Writer(fps=15, metadata=dict(artist='AW'), bitrate=1800)
+ani.save('WE-animation.mp4', writer=mywriter)
