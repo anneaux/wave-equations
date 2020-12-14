2174680d7413d6b92d0871efcb8d6db67e769e0d
anneaux <a.weber@uni-jena.de>
2020-12-14 01:18:51 +0100

    Adding plots directory

--- python/solver.py
+++ python/solver.py
@@ -2,36 +2,25 @@ import numpy as np
 from finite_differences.example_functions import *
 import matplotlib.pyplot as plt
 
-### constants
 c = 1
-
 ### wave equation 0.2
 # d phi / d t = phi
 # d Pi / d t = c^2 d^2 phi / d x^2
 
 ### discretize time and space with uniform grid
-
-endT = 1
-deltat = 0.001
-Nt = int(endT/deltat)
-timevalues = np.linspace(0,endT,Nt)
-
-endX = 1
-deltax = 0.001
-Nx = int(endX/deltax)
-xvalues = np.linspace(0,endX,Nx)
-
-courant = c * deltat / deltax
-print("courant number = %.2f" % courant)
-
 def gridmaker(endT,nt,endX,nx):
-    dt = (endT/nt)
-    timevalues = np.linspace(0,endT,nt)
-    dx = (endX/nx)
-    xvalues = np.linspace(0,endX,nx)
-    # print('dt = %.3f, dx = %.3f' %(dt,dx))
-    return dx,timevalues,dx, xvalues
+  dt = (endT/nt)
+  timevalues = np.linspace(0,endT,nt)
+  dx = (endX/nx)
+  xvalues = np.linspace(0,endX,nx)
+  # print('dt = %.3f, dx = %.3f' %(dt,dx))
+  return dx,timevalues,dx,xvalues
 
+### gaussian wave packet
+def gaussian(x,sigma,mu):
+  return 1/np.sqrt(2*np.pi*sigma**2) * np.exp(- (x-mu)**2/np.sqrt(2*sigma**2))
+def gaussian_drv(x,sigma,mu):
+  return  -(x-mu)/(sigma**2 * np.sqrt(np.pi)) * np.exp(-(x-mu)**2/np.sqrt(2 * sigma**2))
 
 def wave_evolution1D(phi0,Pi0,timevalues,xvalues):
   Nt = len(timevalues)
@@ -54,7 +43,7 @@ def wave_evolution1D(phi0,Pi0,timevalues,xvalues):
   def time_diff(phi, Pi, t): # where u = [phi, Pi]
     dphidt = Pi # because d/dt phi = Pi
     # d/dt Pi = c^2 * d2/dx2 phi, the latter shall be computed using FD (along the discretized x axis)
-    d2phidx2= np.zeros(Nx+2)
+    d2phidx2 = np.zeros(Nx+2)
     for ix in range(1,Nx+1): # computing only inner points
       d2phidx2[ix] = 1/deltax**2 * (phi[ix+1] - 2*phi[ix] + phi[ix-1])
     d2phidx2[-1] = d2phidx2[1]
@@ -79,61 +68,37 @@ def wave_evolution1D(phi0,Pi0,timevalues,xvalues):
 
     phi[i+1,:] = phi[i,:] + deltat*(1/6*k1_phi + 1/3*k2_phi +1/3*k3_phi + 1/6*k4_phi)
     Pi[i+1,:] = Pi[i,:] + deltat*(1/6*k1_Pi + 1/3*k2_Pi +1/3*k3_Pi + 1/6*k4_Pi)
-
   return phi[:,1:Nx+1], Pi[:,1:Nx+1] # return only inner points
 
 
-### gaussian wave packet
-def gaussian(x,sigma,mu):
-  return 1/np.sqrt(2*np.pi*sigma**2) * np.exp(- (x-mu)**2/np.sqrt(2*sigma**2))
-def gaussian_drv(x,sigma,mu):
-  return  -(x-mu)/(sigma**2 * np.sqrt(np.pi)) * np.exp(-(x-mu)**2/np.sqrt(2 * sigma**2))
-
-### example function 4 (sin(12*pi))
-# phi0 = f_4(xvalues)
-# Pi0 = -c*f_4_prime(xvalues)
-
-### example function g(x)
-# phi0 = g_a(xvalues,20)
-# Pi0 = +c * g_a_prime(xvalues,20)
-
-### gaussian wave packet
-phi0 = gaussian(xvalues,0.005,0.5)
-Pi0 = -c * gaussian_drv(xvalues,0.005,0.5)
-
-phi, Pi = wave_evolution1D(phi0,Pi0,timevalues,xvalues)
-
-############################################
-
 #### Plotting the solutions in a position-time diagram ###
 def plot_xt(Nt, deltat, phi_t):
-    times = np.arange(0,Nt*deltat,deltat)
-    plt.plot(times, phi_t)
-    plt.xlabel('t')
-    plt.ylabel('phi(t)')
-    # # plt.axis('square')
-    # # plt.xlim(-12,12)
-    # # plt.ylim(49.94,50.008)
-    # plt.legend()
-    plt.grid(color = 'gainsboro')
-    plt.savefig("plot-phi_t.png")
+  times = np.arange(0,Nt*deltat,deltat)
+  plt.plot(times, phi_t)
+  plt.xlabel('t')
+  plt.ylabel('phi(t)')
+  # # plt.axis('square')
+  # # plt.xlim(-12,12)
+  # # plt.ylim(49.94,50.008)
+  # plt.legend()
+  plt.grid(color = 'gainsboro')
+  plt.savefig("plots/plot-phi_t.png")
 
-### Plotting the time evolution in a diagram with multiple lines
+### Plotting the time evolution in a diagram with multiple lines ###
 from matplotlib import cm
 def plot_xt_evolution(timevalues,xvalues,phi,Nt_plot):
-    Blues = cm.get_cmap('Blues_r',Nt_plot)
+  Blues = cm.get_cmap('Blues_r',Nt_plot)
 
-    for i in np.linspace(0,Nt-1,Nt_plot).astype(int):
-      plt.plot(xvalues, phi[i,:], label ="%.2f s" % timevalues[i], c = Blues(i/Nt))
-    plt.xlabel('x')
-    plt.ylabel('phi(x,t)')
-    # # plt.axis('square')
-    plt.xlim(0, max(xvalues))
-    plt.ylim(np.amin(phi[:,:]),np.amax(phi[:,:]))
-    plt.legend()
-    plt.grid(color = 'gainsboro')
-    plt.savefig("plot-phi(x,t).png")
-
+  for i in np.linspace(0,Nt-1,Nt_plot).astype(int):
+    plt.plot(xvalues, phi[i,:], label ="%.2f s" % timevalues[i], c = Blues(i/Nt))
+  plt.xlabel('x')
+  plt.ylabel('phi(x,t)')
+  # # plt.axis('square')
+  plt.xlim(0, max(xvalues))
+  plt.ylim(np.amin(phi[:,:]),np.amax(phi[:,:]))
+  plt.legend()
+  plt.grid(color = 'gainsboro')
+  plt.savefig("plots/plot-phi(x,t).png")
 
 ### Plotting the time evolution in an animation ######################
 import matplotlib
@@ -162,12 +127,12 @@ def plot_animation(xvalues, timevalues, phi, Pi):
   ani = matplotlib.animation.FuncAnimation(fig3, animate, frames=Nt, blit = True) #init_func=init_animation,
 
   ### write as gif (gif stockt manchmal ein bisschen, geht außerdem sehr langsam zu speichern)
-  ani.save('WE-animation.gif', writer='imagemagick', fps=15)
+  ani.save('plots/WE-animation.gif', writer='imagemagick', fps=15)
 
   ### write as mp4
   Writer = matplotlib.animation.writers['ffmpeg'] # Set up formatting for the movie files
   mywriter = Writer(fps=15, metadata=dict(artist='AW'), bitrate=1800)
-  ani.save('WE-animation.mp4', writer=mywriter)
+  ani.save('plots/WE-animation.mp4', writer=mywriter)
 
 # -------------------- now, do it ---------------
 if __name__ == "__main__":
@@ -175,8 +140,8 @@ if __name__ == "__main__":
     Nt = 100
     endX = 1
     Nx = 100
-    print(deltat)
     deltat, timevalues, deltax, xvalues = gridmaker(endT,Nt,endX,Nx)
+    # print(deltat)
     courant = c * deltat / deltax
     # print("courant number = %.2f" % courant)
 # # choose f_4, f_5, g_a (for latter specify a = ...) or gaussian here (for latter specify sigma and mu)
@@ -189,7 +154,5 @@ if __name__ == "__main__":
     phi, Pi = wave_evolution1D(phi0,Pi0,timevalues,xvalues)
 
     Nt_plot = 5 # how many snap shots are plotted
-    plot_xt_evolution(timevalues,xvalues,phi,Nt_plot)
+    # plot_xt_evolution(timevalues,xvalues,phi,Nt_plot)
     plot_animation(xvalues, timevalues, phi, Pi)
-
-# plot_animation(xvalues, timevalues, phi, Pi)
