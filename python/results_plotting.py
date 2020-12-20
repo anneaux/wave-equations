04974d7cc2789b8254794910045bdf5f5b05acd6
anneaux <a.weber@uni-jena.de>
2020-12-20 18:26:14 +0100

    Add csv-file printing and separate plotting-file

---
+++ python/results_plotting.py
@@ -0,0 +1,76 @@
+import numpy as np
+from finite_differences.example_functions import *
+import matplotlib.pyplot as plt
+from scipy import signal
+
+
+### constants
+c = 1
+
+#### Plotting the solutions in a position-time diagram ###
+def plot_xt(Nt, deltat, phi_t):
+    times = np.arange(0,Nt*deltat,deltat)
+    plt.plot(times, phi_t)
+    plt.xlabel('t')
+    plt.ylabel('phi(t)')
+    # # plt.axis('square')
+    # # plt.xlim(-12,12)
+    # # plt.ylim(49.94,50.008)
+    # plt.legend()
+    plt.grid(color = 'gainsboro')
+    plt.savefig("plots/plot-phi_t.png")
+
+### Plotting the time evolution in a diagram with multiple lines
+from matplotlib import cm
+def plot_xt_evolution(timevalues,xvalues,phi,Nt_plot):
+    Blues = cm.get_cmap('Blues_r',Nt_plot)
+    Nt = len(timevalues)
+    for i in np.linspace(0,Nt-1,Nt_plot).astype(int):
+      plt.plot(xvalues, phi[i,:], label ="%.2f s" % timevalues[i], c = Blues(i/Nt))
+    plt.xlabel('x')
+    plt.ylabel('phi(x,t)')
+    # # plt.axis('square')
+    plt.xlim(0, max(xvalues))
+    plt.ylim(np.amin(phi[:,:]),np.amax(phi[:,:]))
+    plt.legend()
+    plt.grid(color = 'gainsboro')
+    plt.savefig("plots/plot-phi(x,t).png")
+
+
+### Plotting the time evolution in an animation ######################
+import matplotlib
+import matplotlib.animation
+def plot_animation(xvalues, timevalues, phi, Pi):
+  matplotlib.use('Agg')
+  Nt = len(timevalues)
+  def init_animation(xvalues,phi):
+    global line
+    line, = ax.plot(xvalues, np.zeros_like(xvalues))
+    ax.set_xlim(0, max(xvalues))
+    # ax.set_ylim(0,10)
+    ax.set_ylim(np.amin(phi[:,:]),np.amax(phi[:,:]))
+
+  def animate(i):
+    line.set_ydata(phi[i,:])
+    timelabel.set_text('time: %.2f s' % timevalues[i])
+    return line, timelabel
+
+  fig3, ax3 = plt.subplots()
+  ax3.set(xlabel = "x", ylabel = "phi(x)")
+  line, = ax3.plot(xvalues, np.zeros_like(xvalues))
+  # ax3.set_xlim(0, max(xvalues))
+  ax3.set_ylim(np.amin(phi[:,:]),np.amax(phi[:,:]))
+  # ax3.set_ylim(0,10)
+  #, title = "time evolution of 1D wave")
+
+  timelabel = ax3.text(0.02, 0.95, '', transform=ax3.transAxes)
+  ani = matplotlib.animation.FuncAnimation(fig3, animate, frames=Nt, blit = True) #init_func=init_animation,
+
+  # ### write as gif (gif stockt manchmal ein bisschen, geht außerdem sehr langsam zu speichern)
+  # ani.save('plots/WE-animation.gif', writer='imagemagick', fps=15)
+
+  ### write as mp4
+  Writer = matplotlib.animation.writers['ffmpeg'] # Set up formatting for the movie files
+  mywriter = Writer(fps=15, metadata=dict(artist='AW'), bitrate=1800)
+  ani.save('plots/WE-animation.mp4', writer=mywriter)
+
--- python/solver.py
+++ python/solver.py
@@ -1,8 +1,9 @@
 import numpy as np
-from finite_differences.example_functions import *
 import matplotlib.pyplot as plt
 from scipy import signal
 
+from finite_differences.example_functions import *
+from results_plotting import *
 ### constants
 c = 1
 
@@ -12,12 +13,12 @@ c = 1
 
 ### discretize time and space with uniform grid
 def gridmaker(endT,nt,endX,nx):
-    dt = (endT/nt)
-    timevalues = np.linspace(0,endT,nt)
-    dx = (endX/nx)
-    xvalues = np.linspace(0,endX,nx)
-    # print('dt = %.3f, dx = %.3f' %(dt,dx))
-    return dx,timevalues,dx, xvalues
+  dt = (endT/nt)
+  timevalues = np.linspace(0,endT,nt)
+  dx = (endX/nx)
+  xvalues = np.linspace(0,endX,nx)
+  # print('dt = %.3f, dx = %.3f' %(dt,dx))
+  return dx,timevalues,dx, xvalues
 
 
 def wave_evolution1D(phi0,Pi0,timevalues,xvalues,bc):
@@ -122,75 +123,12 @@ def triangle_drv(x,width,mu):
     if x<mu-width:  return 0
     if x>mu-width:  return 0.5
     if x == mu: return 0
+
 ############################################
 
-#### Plotting the solutions in a position-time diagram ###
-def plot_xt(Nt, deltat, phi_t):
-    times = np.arange(0,Nt*deltat,deltat)
-    plt.plot(times, phi_t)
-    plt.xlabel('t')
-    plt.ylabel('phi(t)')
-    # # plt.axis('square')
-    # # plt.xlim(-12,12)
-    # # plt.ylim(49.94,50.008)
-    # plt.legend()
-    plt.grid(color = 'gainsboro')
-    plt.savefig("plots/plot-phi_t.png")
 
-### Plotting the time evolution in a diagram with multiple lines
-from matplotlib import cm
-def plot_xt_evolution(timevalues,xvalues,phi,Nt_plot):
-    Blues = cm.get_cmap('Blues_r',Nt_plot)
-
-    for i in np.linspace(0,Nt-1,Nt_plot).astype(int):
-      plt.plot(xvalues, phi[i,:], label ="%.2f s" % timevalues[i], c = Blues(i/Nt))
-    plt.xlabel('x')
-    plt.ylabel('phi(x,t)')
-    # # plt.axis('square')
-    plt.xlim(0, max(xvalues))
-    plt.ylim(np.amin(phi[:,:]),np.amax(phi[:,:]))
-    plt.legend()
-    plt.grid(color = 'gainsboro')
-    plt.savefig("plots/plot-phi(x,t).png")
-
-
-### Plotting the time evolution in an animation ######################
-import matplotlib
-import matplotlib.animation
-def plot_animation(xvalues, timevalues, phi, Pi):
-  matplotlib.use('Agg')
-  def init_animation(xvalues,phi):
-    global line
-    line, = ax.plot(xvalues, np.zeros_like(xvalues))
-    ax.set_xlim(0, max(xvalues))
-    # ax.set_ylim(0,10)
-    ax.set_ylim(np.amin(phi[:,:]),np.amax(phi[:,:]))
-
-  def animate(i):
-    line.set_ydata(phi[i,:])
-    timelabel.set_text('time: %.2f s' % timevalues[i])
-    return line, timelabel
-
-  fig3, ax3 = plt.subplots()
-  ax3.set(xlabel = "x", ylabel = "phi(x)")
-  line, = ax3.plot(xvalues, np.zeros_like(xvalues))
-  # ax3.set_xlim(0, max(xvalues))
-  ax3.set_ylim(np.amin(phi[:,:]),np.amax(phi[:,:]))
-  # ax3.set_ylim(0,10)
-  #, title = "time evolution of 1D wave")
-
-  timelabel = ax3.text(0.02, 0.95, '', transform=ax3.transAxes)
-  ani = matplotlib.animation.FuncAnimation(fig3, animate, frames=Nt, blit = True) #init_func=init_animation,
-
-  # ### write as gif (gif stockt manchmal ein bisschen, geht außerdem sehr langsam zu speichern)
-  # ani.save('plots/WE-animation.gif', writer='imagemagick', fps=15)
-
-  ### write as mp4
-  Writer = matplotlib.animation.writers['ffmpeg'] # Set up formatting for the movie files
-  mywriter = Writer(fps=15, metadata=dict(artist='AW'), bitrate=1800)
-  ani.save('plots/WE-animation.mp4', writer=mywriter)
 #--------------------- take a look at the energy ------
-def energy(q,p):        #calculate enervy from position q(phi) and inertia p(pi)
+def energy(q,p):        #calculate energy from position q(phi) and inertia p(pi)
     m=1
     E = 0.5* p**2 / m + 0.5* q**2 *m
     return E
@@ -240,8 +178,12 @@ if __name__ == "__main__":
     # print(Pi0)
     Phi, Pi = wave_evolution1D(Phi0,Pi0,timevalues,xvalues, "periodic")
 
-    Etotal = total_energy(Phi,Pi)
-    Nt_plot = 7 # how many snap shots are plotted
-    plot_energy_evolution(Etotal,timevalues)
-    plot_xt_evolution(timevalues, xvalues, Phi, Nt_plot)
-    plot_animation(xvalues, timevalues, Phi, Pi)
+    # Etotal = total_energy(Phi,Pi)
+    # Nt_plot = 7 # how many snap shots are plotted
+    # plot_energy_evolution(Etotal,timevalues)
+    # plot_xt_evolution(timevalues, xvalues, Phi, Nt_plot)
+    # plot_animation(xvalues, timevalues, Phi, Pi)
+
+    # save as csv file
+    # np.savetxt("results.csv", Phi, delimiter = ',', fmt = '%.6e')
+
